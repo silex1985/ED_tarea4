b841d891a9e3cfa5e2d34684a9d0b7f9e70874da
Silvia <silviaexp@hotmail.com>
2023-03-01 00:06:44 +0100

    4.	Encapsular los atributos de la clase CCuenta. Los atributos se utilzarán a través de los getters/setters en vez de directamente

--- src/cuentas/CCuenta.java
+++ src/cuentas/CCuenta.java
@@ -2,7 +2,7 @@ package cuentas;
 
 public class CCuenta {
 
-	 private String nombre;
+		private String nombre;
 	    private String cuenta;
 	    private double saldo;
 	    private double tipoInterés;
@@ -13,21 +13,21 @@ public class CCuenta {
 
 	    public CCuenta(String nom, String cue, double sal, double tipo)
 	    {
-	        nombre =nom;
-	        cuenta=cue;
-	        saldo=sal;
+	        setNombre(nom);
+	        setCuenta(cue);
+	        setSaldo(sal);
 	    }
 
 	    public double estado()
 	    {
-	        return saldo;
+	        return getSaldo();
 	    }
 
 	    public void ingresar(double cantidad) throws Exception
 	    {
 	        if (cantidad<0)
 	            throw new Exception("No se puede ingresar una cantidad negativa");
-	        saldo = saldo + cantidad;
+	        setSaldo(getSaldo() + cantidad);
 	    }
 
 	    public void retirar(double cantidad) throws Exception
@@ -36,6 +36,40 @@ public class CCuenta {
 	            throw new Exception ("No se puede retirar una cantidad negativa");
 	        if (estado()< cantidad)
 	            throw new Exception ("No se hay suficiente saldo");
-	        saldo = saldo - cantidad;
+	        setSaldo(getSaldo() - cantidad);
 	    }
+
+		public String getNombre() {
+			return nombre;
+		}
+
+		public void setNombre(String nombre) {
+			this.nombre = nombre;
+		}
+
+		public String getCuenta() {
+			return cuenta;
+		}
+
+		public void setCuenta(String cuenta) {
+			this.cuenta = cuenta;
+		}
+
+		public double getSaldo() {
+			return saldo;
+		}
+
+		public void setSaldo(double saldo) {
+			this.saldo = saldo;
+		}
+
+		public double getTipoInterés() {
+			return tipoInterés;
+		}
+
+		public void setTipoInterés(double tipoInterés) {
+			this.tipoInterés = tipoInterés;
+		}
+
+
 }
